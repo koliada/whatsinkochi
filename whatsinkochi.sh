ae9b6cc83faf4e706445916c8f27157f589c1ee9
Oleksii Koliada <oleksii.koliada@pipedrive.com>
2017-07-17 00:57:18 +0300

    Print a message when nothing was found

--- whatsinkochi.sh
+++ whatsinkochi.sh
@@ -1,34 +1,40 @@
 #!/bin/sh
 
 if ! hash pup 2>/dev/null; then
-	echo "I require pup, this requires one-time installation...";
-	brew install pup;
+  echo "I need pup, this requires one-time installation...";
+  brew install pup;
 fi
 
-LINE_COUNTER=0;
+URL="http://www.kochiaidad.ee/?go=al20";
 DAY=$(date +'%-d');
+LINE_COUNTER=0;
 
 echo "Figuring out what's in Kochi for today...";
 
-curl -s http://www.kochiaidad.ee/?go=al20 | pup 'p[style="text-align: center;"] > span text{}' | while read line; do
-	if ((LINE_COUNTER > 2)); then
-		echo "--- Head isu! ---";
-		exit 0;
-	fi
+curl -s ${URL} | pup 'p[style="text-align: center;"] > span text{}' | while read line; do
+  if ((LINE_COUNTER > 2)); then
+    echo "--- Head isu! ---";
+    exit 0;
+  fi
 
-	if ((LINE_COUNTER > 0)); then
-		if ((LINE_COUNTER == 1)); then
-			echo "Supp: $line";
-		fi
-		if ((LINE_COUNTER == 2)); then
-			echo "Praad: $line";
-		fi
-		((LINE_COUNTER++));
-		continue;
-	fi
+  if ((LINE_COUNTER > 0)); then
+    if ((LINE_COUNTER == 1)); then
+      echo "Supp: $line";
+    fi
+    if ((LINE_COUNTER == 2)); then
+      echo "Praad: $line";
+    fi
+    ((LINE_COUNTER++));
+    continue;
+  fi
 
-	if echo ${line} | grep -qi "$DAY."; then
-		echo "--- $line ---";
-		((LINE_COUNTER++));
-	fi
+  if echo ${line} | grep -qi "$DAY."; then
+    echo "--- $line ---";
+    ((LINE_COUNTER++));
+  fi
 done
+
+if ((LINE_COUNTER == 0)); then
+  echo "Nothing's found for today. Maybe they haven't updated the menu yet?";
+  exit 0;
+fi
